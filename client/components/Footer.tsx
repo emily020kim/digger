026b3b4e01ed85380d2e1243831c5db043858836
emily020kim <emily020kim@gmail.com>
2024-09-25 19:58:32 -0700

    mobile footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -7,51 +7,51 @@ import { FaLinkedin, FaGithub, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="flex w-full h-full px-4 py-12 border-t-[2px] border-gold">
-      <div className="grid grid-cols-3 w-full">
-        <div className="flex flex-col col-span-1 justify-between">
-          <div className="flex items-center gap-x-3">
+    <div className="flex flex-col md:flex-row w-full h-full px-4 py-12 border-t-[2px] border-gold">
+      <div className="md:grid md:grid-cols-3 w-full">
+        <div className="flex flex-col md:col-span-1 mb-6 md:mb-0">
+          <div className="flex items-center gap-x-3 mb-1">
             <Image src={logo} width={70} height={70} alt="Logo" />
             <Image src={doug} width={70} height={70} alt="Character" />
           </div>
           <div className="flex items-center">
-            <p className="text-xs mr-2">Diggr</p>
+            <p className="text-xs mr-1">Diggr</p>
             <MdOutlineCopyright size={10} />
-            <p className="text-xs ml-2">2024</p>
+            <p className="text-xs ml-1">2024</p>
           </div>
         </div>
-        <div className="flex flex-col col-span-1 gap-y-6">
+        <div className="flex flex-col md:col-span-1 gap-y-2 md:gap-y-6 mb-6 md:mb-0">
           <a
             href="#"
-            className="text-base font-semibold"
+            className="text-sm md:text-base font-semibold"
           >
             FAQs
           </a>
           <a
             href="#"
-            className="text-base font-semibold"
+            className="text-sm md:text-base font-semibold"
           >
             About
           </a>
           <a
             href="#"
-            className="text-base font-semibold"
+            className="text-sm md:text-base font-semibold"
           >
             Support
           </a>
         </div>
-        <div className="flex flex-col col-span-1 gap-y-4">
-          <h1 className="text-xl font-semibold">
+        <div className="flex flex-col md:col-span-1">
+          <h1 className="text-base md:text-xl font-semibold mb-2 md:mb-4">
             Connect with us!
           </h1>
           <div className="flex items-center gap-x-4">
-            <div className="rounded-full p-3 bg-gold">
+            <div className="rounded-full p-2 md:p-3 bg-gold">
               <FaLinkedin size={15} />
             </div>
-            <div className="rounded-full p-3 bg-gold">
+            <div className="rounded-full p-2 md:p-3 bg-gold">
               <FaGithub size={15} />
             </div>
-            <div className="rounded-full p-3 bg-gold">
+            <div className="rounded-full p-2 md:p-3 bg-gold">
               <FaTiktok size={15} />
             </div>
           </div>
