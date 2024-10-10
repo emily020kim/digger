904093fdf3e5a95fecdd987feac1edf6be983cd6
emily020kim <emily020kim@gmail.com>
2024-10-09 19:15:30 -0700

    add links for leaderboards

--- client/components/MusicGenres.tsx
+++ client/components/MusicGenres.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import doug from '../public/facing_left.png';
 
 type GenreCardProps = {
@@ -7,26 +8,29 @@ type GenreCardProps = {
   title: string;
   description: string;
   buttonColor: string;
+  link: string;
 };
 
-const GenreCard: React.FC<GenreCardProps> = ({ bgColor, title, description, buttonColor }) => (
+const GenreCard: React.FC<GenreCardProps> = ({ bgColor, title, description, buttonColor, link }) => (
   <div className={`flex flex-col col-span-1 row-span-1 rounded-md p-4 md:p-6 ${bgColor} gap-y-4 md:gap-y-8`}>
     <h1 className='text-white font-bold text-2xl lg:text-3xl'>{title}</h1>
     <p className='text-white text-xs lg:text-base'>{description}</p>
-    <button className={`w-2/3 bg-white rounded-full py-2 ${buttonColor} text-sm md:text-base lg:text-lg font-semibold`}>
-      See leaderboard
-    </button>
+    <Link href={link}>
+      <button className={`w-2/3 bg-white rounded-full py-2 ${buttonColor} text-sm md:text-base lg:text-lg font-semibold`}>
+        See leaderboard
+      </button>
+    </Link>
   </div>
 );
 
 const MusicGenres = () => {
   const genres = [
-    { title: 'Pop', description: 'Artists like Taylor Swift, Ariana Grande, and Olivia Rodrigo.', bgColor: 'bg-[#FCC5D3]', buttonColor: 'text-[#FCC5D3]' },
-    { title: 'Rap', description: 'Artists like 2Pac, Jay-Z, and Kendrick Lamar.', bgColor: 'bg-[#C23B22]', buttonColor: 'text-[#C23B22]' },
-    { title: 'Band', description: 'Artists like Paramore, Coldplay, and The Killers.', bgColor: 'bg-[#A2BFFE]', buttonColor: 'text-[#A2BFFE]' },
-    { title: 'Indie', description: 'Artists like Phoebe Bridgers, Mitski, and Hozier.', bgColor: 'bg-[#B1D27D]', buttonColor: 'text-[#B1D27D]' },
-    { title: 'International', description: 'Artists like BTS, Blackpink, Bad Bunny, and Rosalia.', bgColor: 'bg-[#D4AF47]', buttonColor: 'text-[#D4AF47]' },
-    { title: 'R&B', description: 'Artists like SZA, Brent Faiyaz, Usher, and Miguel.', bgColor: 'bg-[#341539]', buttonColor: 'text-[#341539]' },
+    { title: 'Pop', description: 'Artists like Taylor Swift, Ariana Grande, and Olivia Rodrigo.', bgColor: 'bg-[#FCC5D3]', buttonColor: 'text-[#FCC5D3]', link: '/leaderboard/pop' },
+    { title: 'Rap', description: 'Artists like 2Pac, Jay-Z, and Kendrick Lamar.', bgColor: 'bg-[#C23B22]', buttonColor: 'text-[#C23B22]', link: '/leaderboard/rap' },
+    { title: 'Band', description: 'Artists like Paramore, Coldplay, and The Killers.', bgColor: 'bg-[#A2BFFE]', buttonColor: 'text-[#A2BFFE]', link: '/leaderboard/band' },
+    { title: 'Indie', description: 'Artists like Phoebe Bridgers, Mitski, and Hozier.', bgColor: 'bg-[#B1D27D]', buttonColor: 'text-[#B1D27D]', link: '/leaderboard/indie' },
+    { title: 'International', description: 'Artists like BTS, Blackpink, Bad Bunny, and Rosalia.', bgColor: 'bg-[#D4AF47]', buttonColor: 'text-[#D4AF47]', link: '/leaderboard/international' },
+    { title: 'Rnb', description: 'Artists like SZA, Brent Faiyaz, Usher, and Miguel.', bgColor: 'bg-[#341539]', buttonColor: 'text-[#341539]', link: '/leaderboard/rnb' },
   ];
 
   return (
@@ -45,6 +49,7 @@ const MusicGenres = () => {
             description={genre.description}
             bgColor={genre.bgColor}
             buttonColor={genre.buttonColor}
+            link={genre.link}
           />
         ))}
       </div>
